db83ab40c29e49b2f31b8acd9ce9902e4e920b37
EmranAli-15 <mohammademranali15@gmail.com>
2023-06-08 21:02:38 +0600

    verifyAdmin and verifyInstructor api created

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ app.use(express.json());
 
 
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2b4mnlf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,12 +48,33 @@ async function run() {
         await client.connect();
 
         const userCollection = client.db("summer-vacation").collection("users");
+        const instructorClassesCollection = client.db("summer-vacation").collection("classes");
 
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.send({ token });
         })
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access' });
+            };
+            next();
+        }
+
+        const verifyInstructor = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden access' });
+            };
+            next();
+        }
 
 
         // user related apis
@@ -46,6 +86,39 @@ async function run() {
                 return res.send({ message: 'user already exist, please login' });
             }
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false });
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' };
+            res.send(result);
+        })
+
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false });
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' };
+            res.send(result);
+        })
+
+        // instructor related apis
+        app.post('/classes', verifyJWT, verifyInstructor, async (req, res) => {
+            const classes = req.body;
+            const result = await instructorClassesCollection.insertOne(classes);
             res.send(result);
         })
 
